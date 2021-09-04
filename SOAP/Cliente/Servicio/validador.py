496c1c934c2817c5e158587fe11040d2532476b3
c.navasm <84421591+CNavasM@users.noreply.github.com>
2021-09-03 20:25:58 -0400

    Update validador.py

--- SOAP/Cliente/Servicio/validador.py
+++ SOAP/Cliente/Servicio/validador.py
@@ -1,9 +1,9 @@
 from itertools import cycle
 
 def rutvalidador (rut):                                     #Recibe un rut como un número entero 
-    reversed_digits = map(int, reversed(str(rut)))         
-    factors = cycle(range(2, 8))
-    s = sum(d * f for d, f in zip(reversed_digits, factors))
+    dígitos = map(int, reversed(str(rut)))         
+    factores = cycle(range(2, 8))
+    s = sum(d * f for d, f in zip(dígitos, factores))
     return (-s) % 11                                        #Retorna el dígito verificador en número entero 
 
 #Si el número entero que regresa es 10, el dígito verificador es K
