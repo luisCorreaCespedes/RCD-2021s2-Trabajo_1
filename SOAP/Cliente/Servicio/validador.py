cecf6787cf89f48565d9295e44e25c560aef37bf
c.navasm <84421591+CNavasM@users.noreply.github.com>
2021-09-03 20:23:40 -0400

    Implementación función Rut
    Se implementó la función validador de Rut para Python, usando la biblioteca itertools.

--- SOAP/Cliente/Servicio/validador.py
+++ SOAP/Cliente/Servicio/validador.py
@@ -1,3 +1,12 @@
+from itertools import cycle
 
-def rutvalidador(int rut):
-def nombrecompleto(str nombre):
+def rutvalidador (rut):                                     #Recibe un rut como un número entero 
+    reversed_digits = map(int, reversed(str(rut)))         
+    factors = cycle(range(2, 8))
+    s = sum(d * f for d, f in zip(reversed_digits, factors))
+    return (-s) % 11                                        #Retorna el dígito verificador en número entero 
+
+#Si el número entero que regresa es 10, el dígito verificador es K
+
+def nombrecompleto (nombre):
+    return
