7480dc402c02f16987c4251043dcdc30e3d48eaf
Luis Alejandro Correa Céspedes <luiscorreacespedes@gmail.com>
2021-09-04 13:27:41 -0400

    carpetas servicios ordenadas

---
+++ SOAP/Servicio/validador.py
@@ -0,0 +1,15 @@
+from itertools import cycle
+
+def rutvalidador (rut):                                     #Recibe un rut como un número entero 
+    dígitos = map(int, reversed(str(rut)))         
+    factores = cycle(range(2, 8))
+    s = sum(d * f for d, f in zip(dígitos, factores))
+    print ((-s) % 11) #Muestra el resultado para comprobar
+    return (-s) % 11                                        #Retorna el dígito verificador en número entero 
+
+#Si el número entero que regresa es 10, el dígito verificador es K
+
+def nombrecompleto (nombre):
+    return
+
+rutvalidador()                    #Pruebasss
